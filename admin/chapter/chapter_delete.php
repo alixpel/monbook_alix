eb46370db51611dfc9873fb0e90940ec72922c0d
Alix Pelletier <alixpelletierpro@gmail.com>
2020-06-14 19:10:41 +0200

    chapter delete

--- admin/chapter/chapter_delete.php
+++ admin/chapter/chapter_delete.php
@@ -1,15 +1,12 @@
 <?php
-// Quand on a cliqué sur le lien "supprimer" dans la liste des chapitress.
 include "../../config.php";
+proteger_page();
 
-proteger_page(); // on ne peut pas acceder à la page sans être connecté.
-
-if(!isset($_GET["chapitreASupprimer"])) { // on verifie que nous avons bien l'identifiant de notre chap à supprimer.
-    ajouterErreur("Merci de choisir le chapitre à supprimer.");
+if(!isset($_GET["chapitreASupprimer"])) {
+    ajouterErreur("Choisir le chapitre à supprimer.");
 } else {
-    // on a l'identifiant, nous supprimons le chap
     $bdd -> query("DELETE FROM chapter WHERE id_projet = " . $_GET["chapitreASupprimer"]);
-    ajouterSuccess("Le chapitre a été supprimé.");
+    show_Success("Le chapitre a été supprimé.");
 
 }
 
