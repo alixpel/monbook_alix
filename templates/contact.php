72490c4be3897ba057157ad4fe419edf8878c4e4
Alix Pelletier <alixpelletierpro@gmail.com>
2020-06-15 18:18:57 +0200

    contact.php css

--- templates/contact.php
+++ templates/contact.php
@@ -3,19 +3,13 @@ include "../config.php";
 include "include/header.php" ?>
 
 <main>
-  <div class="container">
-    <h1><?php echo MontrerValeur("titre_contact")?></h1>
-    <div class="row">
-      <div class="col-6">
-        <div class="pr-20 texte">
-          <?php echo nl2br(MontrerValeur("texte_contact"))?>
-        </div>
-      </div>
-      <div class="col-6">
-        <?php echo html_image("../img/accueil.jpg")?>
-      </div>
-    </div>
-  <div>
+  <h1><?php echo MontrerValeur("TITRE_CONTACT")?></h1>
+  <h3><?php echo nl2br(MontrerValeur("TEXTE_CONTACT")) ?></h3>
+  <div class="contacts">
+    <a href="<?php echo MontrerValeur("TELEPHONE_DIRECT") ?>"><?php echo MontrerValeur("TELEPHONE") ?></a>
+    <a href="<?php echo MontrerValeur("email") ?>">e-mail</a>
+    <a href="<?php echo MontrerValeur("CV") ?>">C.V.</a>
+  </div>
 </main>
 
 <?php include "include/footer.php" ?>
--- templates/include/nav.php
+++ templates/include/nav.php
@@ -1,6 +1,6 @@
 <nav>
   <ul>
-    <li><a href="<?php PATH_TEMPLATE ?>index.php">accueil</a></li>
+    <li><a href="<?php PATH_TEMPLATE ?>accueil.php">accueil</a></li>
     <li><a href="<?php PATH_TEMPLATE ?>templates/">projets</a></li>
     <li><a href="<?php PATH_TEMPLATE ?>contact.php">contact</a></li>
   </ul>
