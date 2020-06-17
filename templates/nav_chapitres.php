4d607dc23828f7fb553dc7515155030b4f666d6f
Alix Pelletier <alixpelletierpro@gmail.com>
2020-06-17 14:13:32 +0200

    fix nav_chapitre.php layout

--- templates/nav_chapitres.php
+++ templates/nav_chapitres.php
@@ -6,16 +6,20 @@ include "include/header.php";
 <div class="nav_chapitres">
 
   <h2>Les projets</h2>
+  <div class="chapter_list">
     <?php
     $requeteChapitres = $bdd->query("SELECT * FROM chapter");
     $resultChapitres = $requeteChapitres->fetchAll();
     foreach ($resultChapitres as $key => $chapitre_a_afficher)  {
       $url_chapitre = "page_chapitre.php?lien=" . $chapitre_a_afficher["id_projet"];
+      echo "<div class='chapter_preview'>";
       echo "<a href='$url_chapitre'>" . $chapitre_a_afficher["nom"] . "</a>";
       echo "<p>" . $chapitre_a_afficher["techno_id"] . "</p>";
       echo html_image("img/chapter_img/" .  $chapitre_a_afficher['id_projet'] . ".jpg", "chapter_thumbnail");
+      echo "</div>";
     }
     ?>
+  </div>
   <h2>Filtrer par technologie</h2>
     <?php
     $requeteTechno = $bdd->query("SELECT * FROM techno");
@@ -24,7 +28,7 @@ include "include/header.php";
       $url_techno = "page_techno.php?lien=" . $techno_a_afficher["id_techno"];
       echo "<a href='$url_techno'>" . $techno_a_afficher["nom_techno"] . "</a>";
     }
-     ?>
+    ?>
 </div>
 
 <?php
