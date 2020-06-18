90d056eeaf3b0cf43be35d7bfea1dc7696f00423
Alix Pelletier <alixpelletierpro@gmail.com>
2020-06-18 15:11:02 +0200

    layout form_accueil files

--- admin/form_accueil/form.php
+++ admin/form_accueil/form.php
@@ -16,17 +16,23 @@ show_success();
   <div class="form">
     <!-- Le type d'encodage des données, enctype, DOIT être spécifié comme ce qui suit -->
     <form enctype="multipart/form-data" action="form_reponse.php" method="post">
+      <hr>
       <div class="field">
-        <input type="text" name="titre" value="<?php echo montrerValeur("TITRE_ACCUEIL")?>" placeholder="Titre de la page d\'accueil" />
+        <label for="titre">Titre</label><input type="text" name="titre" value="<?php echo montrerValeur("TITRE_ACCUEIL")?>" placeholder="Titre de la page d\'accueil" />
       </div>
-      <textarea name="texteAccueil"><?php echo montrerValeur("TEXTE_ACCUEIL")?></textarea>
+      <hr>
+      <div class="field">
+        <label for="texteAccueil">Texte : </label><textarea name="texteAccueil" cols="40" rows="5"><?php echo montrerValeur("TEXTE_ACCUEIL")?></textarea>
+      </div>
+      <hr>
       <input type="hidden" name="MAX_FILE_SIZE" value="5000000" />
       <div class="image_accueil">
         <?php echo html_image("img/accueil.jpg", "mini_image");?>
-      <!-- </div> -->
-      Image de la page d'accueil : <input name="imageAccueil" type="file"  accept="image/jpeg" />
-      <input type="submit" value="Envoyer" />
-      <a href="<?php echo BOOK_URL_SITE ?>admin/accueil.php" class="button">Annuler</a>
+      </div>
+      <label for="imageAccueil">Image de la page d'accueil : </label><input name="imageAccueil" type="file"  accept="image/jpeg" />
+      <hr>
+      <input type="submit" value="Envoyer" class="send-button"/>
+      <a href="<?php echo BOOK_URL_SITE ?>admin/accueil.php" class="cancel-button">Annuler</a>
     </form>
 
   </div>
