119f2b78865bbf5fb11cc64b49c2634e35affa89
Alix Pelletier <alixpelletierpro@gmail.com>
2020-06-20 11:01:54 +0200

    modify instagram link

--- templates/include/footer.php
+++ templates/include/footer.php
@@ -1,7 +1,7 @@
     <footer>
       <div class="reseaux_sociaux">
         <a href="https://www.linkedin.com/in/alix-pelletier-luginb%C3%BChl/" target="_blank"><i class="fab fa-linkedin-in"></i>&nbsplinkedin</a>
-        <a href="https://www.instagram.com/nanapomette/" target="_blank"><i class="fab fa-instagram"></i>&nbspinstagram</a>
+        <a href="https://www.instagram.com/alixpelcode/" target="_blank"><i class="fab fa-instagram"></i>&nbspinstagram</a>
         <a href="#"><i class="fab fa-twitter"></i>&nbsptwitter</a>
       </div>
       <div class="mentions_legales">
